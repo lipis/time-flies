2ad1af477f005e803f598d2bf1979976a2d69c22
Panayiotis Lipiridis <lipiridis@gmail.com>
2024-05-19 22:59:40 +0300

    Google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-ZYFDDTTYQF`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZYFDDTTYQF');
+          `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
